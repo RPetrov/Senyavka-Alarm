4fc316088c916c5b7bf3a1ca76b3bb391ef7703c
rpetrov <roman.al.petrov@yandex.ru>
2017-05-29 10:53:08 +0300

    wakeLock

--- app/src/main/java/rpetrov/senyavkaspeakingalarmclock/AlarmActivity.kt
+++ app/src/main/java/rpetrov/senyavkaspeakingalarmclock/AlarmActivity.kt
@@ -1,17 +1,21 @@
 package rpetrov.senyavkaspeakingalarmclock
 
+import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.PowerManager
 import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.CheckBox
 import android.widget.TextView
 import butterknife.bindView
 import rpetrov.senyavkaspeakingalarmclock.providers.*
 import rpetrov.senyavkaspeakingalarmclock.providers.weather.WeatherProvider
 import java.text.SimpleDateFormat
 import java.util.*
+import android.view.WindowManager
+
+
 
 
 class AlarmActivity : AppCompatActivity() {
@@ -20,9 +24,26 @@ class AlarmActivity : AppCompatActivity() {
     val time: TextView by bindView(R.id.time)
 
     private var providerBuildTask: ProviderBuildTask? = null
+    private var wakeLock: PowerManager.WakeLock? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
+        val o = getSystemService(Context.POWER_SERVICE);
+        if(o is PowerManager){
+            wakeLock = o.newWakeLock(PowerManager.FULL_WAKE_LOCK or
+                    PowerManager.ACQUIRE_CAUSES_WAKEUP or
+                    PowerManager.ON_AFTER_RELEASE, "SEN_ALARM")
+
+            wakeLock?.acquire()
+        }
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON + WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
+                +WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
+                +WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON)
+
+
         setContentView(R.layout.activity_alarm)
 
         val sp: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
@@ -35,12 +56,16 @@ class AlarmActivity : AppCompatActivity() {
     }
 
     fun onCancelClick(view: View): Unit {
+        providerBuildTask?.cancel()
         finish()
     }
 
+    override fun onPause() {
+        super.onPause()
+    }
 
     override fun onStop() {
         super.onStop()
-        providerBuildTask?.cancel()
+        wakeLock?.release()
     }
 }
--- app/src/main/java/rpetrov/senyavkaspeakingalarmclock/AlarmBroadcastReceiver.kt
+++ app/src/main/java/rpetrov/senyavkaspeakingalarmclock/AlarmBroadcastReceiver.kt
@@ -3,6 +3,7 @@ package rpetrov.senyavkaspeakingalarmclock
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.view.WindowManager
 
 
 /**
@@ -14,6 +15,10 @@ class AlarmBroadcastReceiver : BroadcastReceiver() {
 
         val i = Intent(context, AlarmActivity::class.java)
         i.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        i.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED +
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD +
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON +
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON)
         context.startActivity(i)
 
 
