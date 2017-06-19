01acf3ce9005d8cd9b4969895af8c51c436f53c2
rpetrov <roman.al.petrov@yandex.ru>
2017-06-19 16:22:11 +0300

    Summary

--- app/src/main/java/rpetrov/senyavkaspeakingalarmclock/SettingsActivity.kt
+++ app/src/main/java/rpetrov/senyavkaspeakingalarmclock/SettingsActivity.kt
@@ -1,7 +1,10 @@
 package rpetrov.senyavkaspeakingalarmclock
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceActivity
+import android.preference.PreferenceManager
+
 
 /**
  * Created by Roman Petrov
@@ -11,6 +14,18 @@ class SettingsActivity : PreferenceActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         addPreferencesFromResource(intent.getIntExtra("LAYOUT", -1))
+
+        updateSummariesForDatabaseConfig()
     }
 
+
+    private fun updateSummariesForDatabaseConfig() {
+        val sp: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+        for (i in 0..preferenceScreen.preferenceCount - 1) {
+            val preference = preferenceScreen.getPreference(i)
+            preference.summary = (sp.all.get(preference.key)).toString()
+
+        }
+    }
 }
