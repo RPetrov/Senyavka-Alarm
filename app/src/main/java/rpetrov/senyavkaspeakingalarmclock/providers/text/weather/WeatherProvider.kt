f48157aac70c02282be0068ff140be03e69584e4
rpetrov <Roman.Al.petrov@yandex.ru>
2018-10-23 18:24:28 +0300

    check location

--- app/src/main/java/rpetrov/senyavkaspeakingalarmclock/providers/text/weather/WeatherProvider.kt
+++ app/src/main/java/rpetrov/senyavkaspeakingalarmclock/providers/text/weather/WeatherProvider.kt
@@ -2,13 +2,9 @@ package rpetrov.senyavkaspeakingalarmclock.providers.text.weather
 
 import android.Manifest
 import android.content.Context
-import android.content.SharedPreferences
+import android.content.pm.PackageManager
 import android.location.Location
-import android.os.Bundle
-import android.preference.PreferenceManager
-import com.google.android.gms.common.api.GoogleApiClient
-import com.google.android.gms.location.LocationListener
-import com.google.android.gms.location.LocationRequest
+import android.support.v4.content.ContextCompat
 import com.google.android.gms.location.LocationServices
 import com.google.gson.Gson
 import com.ibm.icu.text.RuleBasedNumberFormat
@@ -32,108 +28,53 @@ const val PARAM_CITY: String = "q=%s"
 class WeatherProvider : BaseProvider, ITextProvider {
 
 
-
-   // private val lock = java.lang.Object()
+    // private val lock = java.lang.Object()
     var result: Result? = null
+    var location: Location? = null
 
     constructor(context: Context) : super(context)
+    private val lock = java.lang.Object()
+
+    override fun prepare(): Boolean {
+
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+
+        if(ContextCompat.checkSelfPermission(context,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED){
+
+            fusedLocationClient.lastLocation
+                    .addOnSuccessListener { location: Location? ->
+                        this.location = location
+                        synchronized(lock) {lock.notifyAll()}
+                    }
 
 
-    override fun prepare(): Boolean  {
-
-//        var mGoogleApiClient: GoogleApiClient? = null
-//
-//        mGoogleApiClient = GoogleApiClient.Builder(context)
-//                .addConnectionCallbacks(object : GoogleApiClient.ConnectionCallbacks {
-//                    override fun onConnected(p0: Bundle?) {
-//                        val mLastLocation = try {
-//                            LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient)
-//                        }  catch (e: SecurityException) { null }
-//
-//                        if (mLastLocation != null) {
-//
-//                            getWeatherByLocation(mLastLocation)
-//
-//                        } else {
-//
-//                            val mLocationRequest = LocationRequest()
-//                            mLocationRequest.interval = 10000
-//                            mLocationRequest.fastestInterval = 5000
-//                            mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-//
-//                            try {
-//                                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, object : LocationListener {
-//                                    override fun onLocationChanged(p0: Location?) {
-//                                        if(p0 != null)
-//                                            getWeatherByLocation(p0)
-//                                        else{
-//                                            val loc = Location("MY_LOC")
-//                                            val sp: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
-//                                            loc.latitude = sp.getFloat("weather_lan", -1f).toDouble()
-//                                            loc.longitude = sp.getFloat("weather_lon", -1f).toDouble()
-//
-//                                            if(loc.latitude == -1.0 || loc.longitude == -1.0){
-//                                                getWeatherByAddress(sp.getString("weather_city", null))
-//                                            } else{
-//                                                getWeatherByLocation(loc)
-//                                            }
-//
-//
-//                                        }
-//
-//
-//                                        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this)
-//                                    }
-//                                })
-//                            } catch(e: SecurityException) {
-//                                synchronized(lock) {
-//                                    lock.notifyAll()
-//                                }
-//                            }
-//                        }
-//                    }
-//
-//                    override fun onConnectionSuspended(p0: Int) {
-//                        System.out.print(p0)
-//                    }
-//                })
-//                .addOnConnectionFailedListener({ })
-//                .addApi(LocationServices.API)
-//                .build()
-//
-//        mGoogleApiClient.connect()
-
-//        synchronized(lock) {
-//            lock.wait(90000)
-//        }
-
-        if(result == null){
-//            val loc = Location("MY_LOC")
-            val sp: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
-//            loc.latitude = sp.getFloat("weather_lan", -1f).toDouble()
-//            loc.longitude = sp.getFloat("weather_lon", -1f).toDouble()
-
- //           if(loc.latitude == -1.0 || loc.longitude == -1.0){
- //               result = getWeatherByAddress(sp.getString("weather_city", "Saint-Petersburg"))
- //           } else{
-                result = getWeatherByLocation()
-//            }
+            synchronized(lock) {
+                lock.wait(90000)
+            }
         }
 
+        val location = this.location
+        result = if(location != null){
+            getWeatherByLocation(location.latitude, location.longitude)
+        } else{
+            getWeatherByLocation(59.934788, 30.275356)
+        }
 
         return result != null
     }
 
 
-    private fun getWeatherByLocation() : Result{
+    private fun getWeatherByLocation(lat : Double, lon : Double): Result {
         val gson: Gson = Gson()
-        val param = String.format(PARAM_LOCATION, 59.934788, 30.275356)
+        val param = String.format(PARAM_LOCATION, lat, lon)
         return gson.fromJson(BufferedReader(InputStreamReader(java.net.URL(String.format(URL, param)).openStream())), Result::class.java)
 
 
     }
 
-    private fun getWeatherByAddress(city: String) : Result {
+    private fun getWeatherByAddress(city: String): Result {
         val gson: Gson = Gson()
         val param = String.format(PARAM_CITY, city)
         return gson.fromJson(BufferedReader(InputStreamReader(java.net.URL(String.format(URL, param)).openStream())), Result::class.java)
@@ -141,24 +82,24 @@ class WeatherProvider : BaseProvider, ITextProvider {
 
     override fun getText(): String {
 
-        val res =result ?: return ""
+        val res = result ?: return ""
 
-        var text: String = "Сейчас " + res.main.temp.toInt() + " " + Utils.getCorrectWordForDigit(res.main.temp.toInt(), " градус", " градуса", " градусов", " градусов") +  ". "
+        var text: String = "Сейчас " + res.main.temp.toInt() + " " + Utils.getCorrectWordForDigit(res.main.temp.toInt(), " градус", " градуса", " градусов", " градусов") + ". "
 
-        text+= " " + res.weather.first().description + ". "
+        text += " " + res.weather.first().description + ". "
 
-        if(res.wind != null){
-            val windVar:Wind = res.wind
+        if (res.wind != null) {
+            val windVar: Wind = res.wind
 
             val ruleBasedNumberFormat = RuleBasedNumberFormat(Locale("ru"), RuleBasedNumberFormat.SPELLOUT)
-            text+= " Скорость ветра " + ruleBasedNumberFormat.format(windVar.speed.toInt()) + Utils.getCorrectWordForDigit(windVar.speed.toInt(), " метр", " метра", " метров", " метров") + " в секунду."
+            text += " Скорость ветра " + ruleBasedNumberFormat.format(windVar.speed.toInt()) + Utils.getCorrectWordForDigit(windVar.speed.toInt(), " метр", " метра", " метров", " метров") + " в секунду."
         }
 
         return text
 
     }
 
-    override fun getConfigLayout(): Int  = R.xml.weather
+    override fun getConfigLayout(): Int = R.xml.weather
 
     override fun getPermissions(): Array<String> = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
 
