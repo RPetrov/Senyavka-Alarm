a84c65cea147b2cb75ca5068dc15bb4e538dfdc2
rpetrov <Roman.Al.petrov@yandex.ru>
2019-06-21 16:29:58 +0300

    Fix for currency rate provider

--- app/src/main/java/rpetrov/senyavkaspeakingalarmclock/providers/text/CurrencyProvider.kt
+++ app/src/main/java/rpetrov/senyavkaspeakingalarmclock/providers/text/CurrencyProvider.kt
@@ -1,7 +1,9 @@
 package rpetrov.senyavkaspeakingalarmclock.providers.text
 
 import android.content.Context
+import com.google.gson.annotations.SerializedName
 import com.ibm.icu.text.RuleBasedNumberFormat
+
 
 /**
  * Created by Roman Petrov
@@ -9,33 +11,62 @@ import com.ibm.icu.text.RuleBasedNumberFormat
 class CurrencyProvider : BaseProvider, rpetrov.senyavkaspeakingalarmclock.providers.ITextProvider {
     constructor(context: Context) : super(context)
 
+    data class Rates(
+            @SerializedName("Date")
+            val date: String,
+            @SerializedName("PreviousDate")
+            val previousDate: String,
+            @SerializedName("PreviousURL")
+            val previousURL: String,
+            @SerializedName("Timestamp")
+            val timestamp: String,
+            @SerializedName("Valute")
+            val valute: Map<String, Rate>
+    )
 
-    data class Rates(val RUB: String)
-    data class Result(val rates: rpetrov.senyavkaspeakingalarmclock.providers.text.CurrencyProvider.Rates)
+    data class Rate(
+            @SerializedName("CharCode")
+            val charCode: String,
+            @SerializedName("ID")
+            val iD: String,
+            @SerializedName("Name")
+            val name: String,
+            @SerializedName("Nominal")
+            val nominal: Int,
+            @SerializedName("NumCode")
+            val numCode: String,
+            @SerializedName("Previous")
+            val previous: Double,
+            @SerializedName("Value")
+            val value: Double
+    )
 
-    var result: rpetrov.senyavkaspeakingalarmclock.providers.text.CurrencyProvider.Result? = null
+    lateinit var preparedText: String
 
     override fun prepare(): Boolean {
 
         val gson: com.google.gson.Gson = com.google.gson.Gson()
-        result = gson.fromJson(java.io.BufferedReader(java.io.InputStreamReader(java.net.URL("http://api.fixer.io/latest?base=USD").openStream())), rpetrov.senyavkaspeakingalarmclock.providers.text.CurrencyProvider.Result::class.java)
+        val result = gson.fromJson(java.io.BufferedReader(java.io.InputStreamReader(java.net.URL("https://www.cbr-xml-daily.ru/daily_json.js").openStream())), rpetrov.senyavkaspeakingalarmclock.providers.text.CurrencyProvider.Rates::class.java)
 
+        // val r = "%2d".format(result.valute.get("USD")?.value?.toInt())
+        val r = result.valute.get("USD")?.value?.toInt()
+        val k = ((result.valute.get("USD")?.value!! - (result.valute.get("USD") as Rate).value.toInt()) * 100).toInt()
+
+        if (r == null || k == null)
+            return false
+
+        var ruleBasedNumberFormat = com.ibm.icu.text.RuleBasedNumberFormat(java.util.Locale("ru"), RuleBasedNumberFormat.SPELLOUT)
+
+        var text: String = "Курс доллара " + ruleBasedNumberFormat.format(r) + " " + rpetrov.senyavkaspeakingalarmclock.providers.Utils.getCorrectWordForDigit(r, " рубль", " рубля", " рублей", " рублей")
+        text += ", " + ruleBasedNumberFormat.format(k.toLong()) + " " + rpetrov.senyavkaspeakingalarmclock.providers.Utils.getCorrectWordForDigit(k.toInt(),
+                " копейка", " копейки", " копеек", " копеек")
+
+        preparedText = text
         return result != null
     }
 
     override fun getText(): String {
-        val res = result!!
-
-        val r = "%2d".format(res.rates.RUB.toDouble().toInt())
-        val k = "%2d".format(((res.rates.RUB.toDouble() - res.rates.RUB.toDouble().toInt())*100).toInt())
-
-        var ruleBasedNumberFormat = com.ibm.icu.text.RuleBasedNumberFormat(java.util.Locale("ru"), RuleBasedNumberFormat.SPELLOUT)
-
-        var text: String = "Курс доллара " + ruleBasedNumberFormat.format(r.toLong()) + " "  + rpetrov.senyavkaspeakingalarmclock.providers.Utils.getCorrectWordForDigit(res.rates.RUB.toDouble().toInt(), " рубль", " рубля", " рублей", " рублей")
-        text += ". " + ruleBasedNumberFormat.format(k.toLong()) + " " + rpetrov.senyavkaspeakingalarmclock.providers.Utils.getCorrectWordForDigit((res.rates.RUB.toDouble() - res.rates.RUB.toDouble().toInt() * 100).toInt(),
-                " копейка", " копейки", " копеек", " копеек")
-
-        return text
+        return preparedText
     }
 
     override fun getConfigLayout(): Int {
